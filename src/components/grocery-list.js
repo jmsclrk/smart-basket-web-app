f8519104a875c3074ef34a9ed9efd4d1b3e790a1
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2020-01-27 16:27:16 +0000

    Grocery list will display a single grocery passed via props

--- src/components/grocery-list.js
+++ src/components/grocery-list.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function GroceryList() {
-  return <h1>No groceries available.</h1>;
+export default function GroceryList(props) {
+  let groceries = props.groceries
+  if (groceries) {
+    return (
+      <div>
+        <h2>{groceries[0].name} - {groceries[0].amount} {groceries[0].measurement}</h2>
+        <strong>{groceries[0].price}</strong>
+      </div>
+    )
+  } else {
+    return <h1>No groceries available.</h1>;
+  }
 }
--- src/components/grocery-list.test.js
+++ src/components/grocery-list.test.js
@@ -16,9 +16,21 @@ afterEach(() => {
   container = null;
 });
 
+const oneGrocery = [{
+  name: "Carrots",
+  amount: 500,
+  measurement: "grams",
+  price: 1
+}]
+
 it("renders with or without a groceries array", () => {
   act(() => {
     render(<GroceryList />, container);
   });
   expect(container.textContent).toBe("No groceries available.");
+
+  act(() => {
+    render(<GroceryList groceries={oneGrocery}/>, container);
+  });
+  expect(container.textContent).toContain("Carrots");
 });
