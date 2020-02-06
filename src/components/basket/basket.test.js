dfc3f16a392195fa122d39fd64efbd6e7face6c6
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2020-02-06 16:02:14 +0000

    Adds tests for when basket has items

--- src/components/basket/basket.test.js
+++ src/components/basket/basket.test.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Basket from ".";
-import { testSingleGroceryBasket } from './../config/test-groceries'
+import { testSingleGroceryBasket, testMultiGroceryBasket } from './../config/test-groceries'
 import { shallow } from 'enzyme'
 import { findByDataTestAttr } from './../config/test-utils'
 import { testStore } from './../../../Utils'
-
-
 
 const setUp = (initialState={}) => {
   const store = testStore(initialState)
@@ -37,6 +35,30 @@ describe('Basket Component', () => {
       const message = findByDataTestAttr(component, 'bskt-empty-message')
       expect(message.length).toBe(1)
       expect(message.text()).toBe('Nothing added yet.')
+    })
+  })
+
+  describe('Basket is not empty', () => {
+
+    let component
+    beforeEach(() => {
+      component = setUp({basketContents: testSingleGroceryBasket})
+    })
+
+    it('Should render basket component without errors', () => {
+      const wrapper = findByDataTestAttr(component, 'bskt-component')
+      expect(wrapper.length).toBe(1)
+    })
+
+    it('Should render basket title', () => {
+      const title = findByDataTestAttr(component, 'bskt-title')
+      expect(title.length).toBe(1)
+      expect(title.text()).toBe('BASKET')
+    })
+
+    it('Should NOT render "Nothing added yet." message', () => {
+      const message = findByDataTestAttr(component, 'bskt-empty-message')
+      expect(message.length).toBe(0)
     })
   })
 })
