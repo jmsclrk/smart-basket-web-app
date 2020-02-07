ef3eac28627e9811726db0c34a043537f24eeb3a
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2020-02-07 09:44:50 +0000

    Updates baskedContents reducer to return new state not mutated state

--- src/reducers/basketContents/index.js
+++ src/reducers/basketContents/index.js
@@ -1,13 +1,13 @@
 export default (state=[], action) => {
   switch(action.type){
     case 'addItemToBasket':
-      const bIndex = state.findIndex(bItem => bItem.grocery === action.payload)
+      const bIndex = state.findIndex(bItem => bItem.grocery.id === action.payload.id)
       if(bIndex >= 0){
         state[bIndex].quantity += 1
       } else {
         state.push({quantity: 1, grocery: action.payload})
       }
-      return state
+      return Object.assign([], state)
     default:
       return state
   }
